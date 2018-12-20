1d5d04aec2fd2594b621f180ca33c1eae8066dc1
Tatiana Molina <tatymoly@Tatianas-MacBook-Air.local>
2018-12-20 16:47:30 -0500

    new card

--- bin/card.js
+++ bin/card.js
@@ -13,38 +13,43 @@ let options = {
 };
 // Text + chalk definitions
 let data = {
-  name: chalk.white('Tatiana Molina /'),
-  handle: chalk.cyan('tatymolys - tatymoly'),
-  work: chalk.white('Senior Web Engineer at Deloitte'),
-  twitter: chalk.cyan('https://twitter.com/tatymolys'),
-  github: chalk.cyan('https://github.com/tatymoly'),
-  linkedin: chalk.cyan('https://www.linkedin.com/in/tatymoly/'),
-  web: chalk.cyan('https://tatymoly.me/'),
-  medium: chalk.cyan('https://medium.com/@tatymolys'),
+  name: chalk.hex('#45e7fa')('Tatiana Molina ~'),
+  handle: chalk.hex('#fa5845')('tatymolys'),
+  description: chalk.hex('#fab345')('I dont like avocado toast 🥑 🚫 🍞'),
+  work: chalk.hex('#45e7fa')('Web Engineer at Deloitte'),
+  twitter: chalk.hex('#fa5845')('https://twitter.com/tatymolys'),
+  github: chalk.hex('#fa5845')('https://github.com/tatymoly'),
+  linkedin: chalk.hex('#fa5845')('https://www.linkedin.com/in/tatymoly/'),
+  web: chalk.hex('#fa5845')('https://tatymoly.me/'),
+  medium: chalk.hex('#fa5845')('https://medium.com/@tatymolys'),
   npx: chalk.white('npx tatymolys'),
   labelWork: chalk.white.bold('      Work:'),
   labelTwitter: chalk.white.bold('   Twitter:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
   labelLinkedIn: chalk.white.bold('  LinkedIn:'),
   labelWeb: chalk.white.bold('       Web:'),
-  labelMedium: chalk.white.bold('       Medium:'),
+  labelMedium: chalk.white.bold('    Medium:'),
   labelCard: chalk.white.bold('      Card:'),
 };
 
 // Actual strings we're going to output
 var newline = '\n';
 var heading = `${data.name} ${data.handle}`;
+var description = `${data.description}`;
 var working = `${data.labelWork}  ${data.work}`;
 var twittering = `${data.labelTwitter}  ${data.twitter}`;
 var githubing = `${data.labelGitHub}  ${data.github}`;
 var linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
 var webing = `${data.labelWeb}  ${data.web}`;
-var medium = `${data.labelMedium} ${data.medium}`;
+var medium = `${data.labelMedium}  ${data.medium}`;
 var carding = `${data.labelCard}  ${data.npx}`;
 
 // Put all our output together into a single variable so we can use boxen effectively
 let output =
   heading +
+  newline +
+  newline +
+  description +
   newline +
   newline +
   working +
@@ -55,9 +60,11 @@ let output =
   newline +
   linkedining +
   newline +
+  medium +
+  newline +
   webing +
   newline +
   newline +
   carding;
 
-console.log(chalk.green(boxen(output, options)));
+console.log(chalk.hex('#45e7fa')(boxen(output, options)));
